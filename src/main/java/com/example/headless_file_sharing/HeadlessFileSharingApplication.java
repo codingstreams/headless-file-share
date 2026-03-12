a7b1fcfcc90ba74a8a610e3e71f932da4d3382cf
codingstreams <akxhdev@outlook.com>
2026-03-12 15:41:32 +0530

    init commit

---
+++ src/main/java/com/example/headless_file_sharing/HeadlessFileSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.headless_file_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlessFileSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlessFileSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/headless_file_sharing/HeadlessFileSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.headless_file_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadlessFileSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
